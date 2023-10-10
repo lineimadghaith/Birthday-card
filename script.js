7b4e639aad371dac579b6a0e4a1e30ee4f5b429f
lineimadghaith <123649910+lineimadghaith@users.noreply.github.com>
2023-10-10 11:25:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    document.getElementById("myP").style.display = "none";
+    document.getElementById("second").style.display= "block";
+}
+function function2() {
+    document.getElementById("second").style.display= "none";
+    document.getElementById("myP").style.display= "block";
+}
